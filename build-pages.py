a30ab90ec6416f811e9bee11a3dbe5c2556412d3
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-04-15 15:23:40 -0500

    Print progress while building pages

--- build-pages.py
+++ build-pages.py
@@ -8,4 +8,5 @@ for ocr_id in ocremixdata.get_remix_ids(cnx):
     target = pathlib.Path(f'output/remix/OCR{ocr_id:05}.json')
     target.parent.mkdir(parents=True, exist_ok=True)
     with target.open('w') as f:
+        print(f'writing to {target}')
         json.dump(ocremixdata.get_remix_data(cnx, ocr_id), f, indent=4, sort_keys=True)
