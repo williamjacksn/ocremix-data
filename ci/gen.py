2f78dd7f97248bc773414d2f4e5f789a1f383382
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-08-13 10:30:02 -0500

    Update yaml gen script

--- ci/gen.py
+++ ci/gen.py
@@ -1,8 +1,39 @@
 import json
 import pathlib
 
+THIS_FILE = pathlib.PurePosixPath(
+    pathlib.Path(__file__).relative_to(pathlib.Path().resolve())
+)
+ACTIONS_CHECKOUT = {"name": "Check out repository", "uses": "actions/checkout@v5"}
+
 
 def gen(content: dict, target: str):
+    pathlib.Path(target).parent.mkdir(parents=True, exist_ok=True)
     pathlib.Path(target).write_text(
         json.dumps(content, indent=2, sort_keys=True), newline="\n"
     )
+
+
+def gen_dependabot():
+    target = ".github/dependabot.yaml"
+    content = {
+        "version": 2,
+        "updates": [
+            {
+                "package-ecosystem": e,
+                "allow": [{"dependency-type": "all"}],
+                "directory": "/",
+                "schedule": {"interval": "daily"},
+            }
+            for e in ["github-actions", "uv"]
+        ],
+    }
+    gen(content, target)
+
+
+def main():
+    gen_dependabot()
+
+
+if __name__ == "__main__":
+    main()
