f3a91c8992331e1f6167f5db68562fc0c8d76624
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-08-13 10:23:03 -0500

    Make commit message configurable in workflow

--- ci/commit-and-push.sh
+++ ci/commit-and-push.sh
@@ -1,5 +1,5 @@
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 git add --all
-git commit --message="Import missing ReMix info" && echo "pushed=true" >> $GITHUB_OUTPUT || exit 0
+git commit --message="${COMMIT_MESSAGE}" && echo "pushed=true" >> $GITHUB_OUTPUT || exit 0
 git push
