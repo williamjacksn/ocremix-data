278ed968f9e6198cc66632b95b7352e17114e64e
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-11-04 12:22:06 -0600

    Update swagger-ui-dist dependency to version 5.30.1

--- ci/gen.py
+++ ci/gen.py
@@ -38,7 +38,7 @@ def gen_package_json() -> None:
         "version": "1.0.0",
         "license": "UNLICENSED",
         "private": True,
-        "dependencies": {"swagger-ui-dist": "5.29.5"},
+        "dependencies": {"swagger-ui-dist": "5.30.1"},
     }
     gen(content, target)
 
